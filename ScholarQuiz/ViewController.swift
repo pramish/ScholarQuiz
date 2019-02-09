ee785ea224d7d39115dd92a08df29653c2eb7a9b
Pramish <luitelpramish7@gmail.com>
2019-02-09 19:47:16 +1100

    Initial Commit

---
+++ ScholarQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScholarQuiz
+//
+//  Created by Pramish Luitel on 9/2/19.
+//  Copyright © 2019 Pramish Luitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
